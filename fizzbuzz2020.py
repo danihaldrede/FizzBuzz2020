8c0cf5e39aa260e371b477d5308c45ef21c6184b
danihaldrede <59898921+danihaldrede@users.noreply.github.com>
2020-02-05 11:32:43 -0500

    Update fizzbuzz2020.py

--- fizzbuzz2020.py
+++ fizzbuzz2020.py
@@ -1,1 +1,13 @@
 # Fizzbuzz2020
+
+for fizzbuzz in range(100):
+  if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+    print("fizzbuzz")
+    continue
+  elif fizzbuzz % 3 == 0: 
+    print("fizz")
+    continue
+  elif fizzbuzz % 5 == 0: 
+    print("buzz")
+    continue
+  print(fizzbuzz)
